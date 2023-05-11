5669bbd30abfb2218596d57bc4ac173045782852
juanferGG <juanfe15777@gmail.com>
2023-05-11 08:56:45 -0500

    vf

--- MorochoAPI/js/app.js
+++ MorochoAPI/js/app.js
@@ -2,10 +2,28 @@ const btn = document.getElementById('btn')
 
 // API Key KADZwEqjkTxFLi8E1LnA5gJnC2i0REIzUk0grKeG6Ao
 
+// HTML
+
+const imgAPI = document.getElementById('imgAPI')
+const description = document.getElementById('description')
+const created_at = document.getElementById('created_at')
+
+
 
 async function getMethod(){
-    const data = await fetch('https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&w=1500&dpr=2').then((e) => e.json())
-    console.log(data)
+    const data = await fetch(`https://api.unsplash.com/photos/?client_id=KADZwEqjkTxFLi8E1LnA5gJnC2i0REIzUk0grKeG6Ao`).then((e) => e.json())
+    const dataRandom = Math.floor(Math.random() * data.length)
+
+    imgAPI.style.backgroundImage = `url('${data[dataRandom].urls.full}')`
+    description.textContent = `DEscription: ${data[dataRandom].alt_description}`
+    created_at.textContent = `Date: ${data[dataRandom].created_at}`
+
+
+    console.log(data[dataRandom].urls.full)
+    console.log(data[dataRandom].alt_description)
+    console.log(data[dataRandom].created_at)
+
+
 }
 
 btn.addEventListener('click', getMethod)
